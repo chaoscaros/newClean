642626df8d85a54454b7d4bdb3d66a80cc3d1b4a
zhuangcx <zcx123456>
2022-06-23 09:08:54 +0800

    CFD BUG修复

--- jd_cfd_hb.py
+++ jd_cfd_hb.py
@@ -229,7 +229,7 @@ def set_arr_config(tempArr_times,tempArr_status):
             put_envs(startArr_dist.get('_id'), startArr_dist.get('name'), str(tempArr_times)[:8])
         print('更新财富岛兑换起始时间范围变量[{}]'.format(str(tempArr_times)[:8]))
     elif tempArr_status == 2013 :
-        if endArr_times < tempArr_times :
+        if endArr_time < tempArr_times :
             put_envs(endArr_dist.get('_id'), endArr_dist.get('name'), str(tempArr_times)[:8])
         print('更新财富岛兑换结束时间范围变量[{}]'.format(str(tempArr_times)[:8]))
     
